a2960802187300b082409313e56ca928a98c0403
Stefan Adamovic <stef.adamovic@gmail.com>
2024-02-13 17:52:18 +0100

    Initial commit

---
+++ src/test/java/com/asioso/stefan/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asioso.stefan.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
